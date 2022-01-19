e391a37ee4f1f669df372655404f1e036c5de439
ericpretzel <ewetzel@ucsb.edu>
2022-01-18 22:03:14 -0800

    basic scraper for csgostats.gg

--- extension/csgo.py
+++ extension/csgo.py
@@ -1,54 +1,52 @@
 import config
 import discord
 from discord.ext import commands
-from discord.commands import slash_command
-import requests
-import json
-import pprint
+from discord.commands import slash_command, Option
+from scraper import get_stats
 
-stats = [
-    'timePlayed',
-    'kills',
-    'deaths',
-    'kd',
-    'damage',
-    'bombsPlanted',
-    'bombsDefused',
-    'mvp',
-    'wins',
-    'ties',
-    'losses',
-    'wlPercentage',
-    'roundsPlayed',
-    'headshotPct'
-]
-def _get_stat(r, stat):
-    return r['data']['segments'][0]['stats'][stat]['value']
-
-def _adr(r):
-    return int(_get_stat(r, 'damage')) / int(_get_stat(r, 'roundsPlayed'))
+# display value : json value
+stat_values = {
+    'ADR': 'adr',
+    'HS %': 'hs',
+    'K/D': 'kpd',
+    'Rating': 'rating',
+    'Win %': 'wr'
+}
 
 class CSGO(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @slash_command(guild_ids=[config.WIDMARK_CLAN_GUILD_ID])
-    async def csgostats(self, ctx: discord.ApplicationContext, user: str):
-        url = f'https://public-api.tracker.gg/v2/csgo/standard/profile/steam/{user}'
-        headers = {'TRN-Api-Key': config.TRN_API_KEY}
-        r = requests.get(url=url, headers=headers).json()
+    @slash_command(guild_ids=[config.WIDMARK_CLAN_GUILD_ID], 
+        description="Retrieve a player's CSGO stats.")
+    async def csgostats(self,
+        ctx: discord.ApplicationContext,
+        username: Option(str, "Custom profile name", required=False, default=None),
+        url: Option(str, "Steam profile URL", required=False, default=None)):
 
-        embed=discord.Embed(title=f"CS:GO stats for user {user}", description="from tracker.gg")
-        embed.add_field(name="K/D", value=int(_get_stat(r, 'kills'))/int(_get_stat(r, 'deaths')), inline=True)
-        embed.add_field(name="Win %", value=_get_stat(r, 'wlPercentage'), inline=True)
-        embed.add_field(name='ADR', value=_adr(r), inline=True)
-        embed.add_field(name="Headshot %", value=_get_stat(r, 'headshotPct'), inline=True)
-        embed.add_field(name="Time Played", value=_get_stat(r, 'timePlayed'), inline=True)
+        if not username and not url:
+            return await ctx.respond('No user specified.')
         
+        if url:
+            if url[-1] == '/':
+                url = url[:-1]
+            user = url[url.rindex('/')+1:]
+        elif username:
+            user = username
+
+        await ctx.defer() # need to defer so the command doesn't time out
+
+        try:
+            stats = get_stats(user)
+        except Exception as e:
+            return await ctx.respond(e.args[0])
+
+        embed=discord.Embed(title=f"CS:GO stats for user {user}", description="from csgostats.gg")
+        for k, v in stat_values.items():
+            embed.add_field(name=k, value=stats['overall'][v], inline=True)
+        embed.set_thumbnail(url=stats['thumbnail'])
+
         await ctx.respond(embed=embed)
-
-
-
 
 def setup(bot):
     bot.add_cog(CSGO(bot))
--- scraper.py
+++ scraper.py
@@ -1,34 +1,48 @@
-from pprint import pprint
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import re
 import json
 
-find_stats = re.compile('var stats = {.+};')
+find_thumbnail = re.compile('src="(https:\\/\\/.+_full.jpg)"')
+find_id = re.compile('"(\\d{17})"')
+find_stats = re.compile('var stats = ({.+});')
 
 # configure settings and open website
 options = Options()
 options.add_argument('--enable-javascript')
 options.add_argument('--headless')
 
-driver = webdriver.Chrome(options=options)
+def get_stats(query):
+    driver = webdriver.Chrome(options=options)
 
-# workaround for being detected as a bot
-# got this from https://stackoverflow.com/a/69766804
-driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    # workaround for being detected as a bot
+    # got this from https://stackoverflow.com/a/69766804
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
-driver.get('https://csgostats.gg/player/76561198106229202')
+    # find the steamID of the user
+    driver.get(f'https://steamid.xyz/{query}')
+    result = find_id.search(driver.page_source)
+    if not result:
+        raise Exception('Could not find user.')
+    id = result.group(1)
 
-result = find_stats.search(driver.page_source)
-if not result:
-    print("couldn't find stats!")
-    exit()
+    result = find_thumbnail.search(driver.page_source)
+    if not result:
+        raise Exception('Could not find user.')
+    thumbnail = result.group(1)
 
-stats = result.group(0)
-stats = json.loads(stats[len('var stats = '):-1])
+    driver.get(f'https://csgostats.gg/player/{id}')
+    result = find_stats.search(driver.page_source)
+    if not result:
+        raise Exception('Could not find user on csgostats.gg')
 
-with open('output.txt', 'w') as f:
-    pprint(stats, f)
+    stats = json.loads(result.group(1))
+    stats['thumbnail'] = thumbnail
 
-driver.close()
+    driver.close()
+    return stats
