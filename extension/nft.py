2602e801b79f590c07ddcc0d5865bfa9f908adf8
Lance Math <l.a.mathia1@gmail.com>
2022-01-22 05:23:51 -0800

    nft generator

--- extension/nft.py
+++ extension/nft.py
@@ -1,95 +1,59 @@
+"""
+smart_contract.py 
+By Lance Mathias <l.a.mathia1@gmail.com>
+Infrastructure for the Widmark Clan crypto branch NFT exchange
+"""
+
 import config
 import discord
 from discord.commands import slash_command
 from discord.ext import commands
-from PIL import Image, ImageChops
-from numpy.random import normal, randint
-from os import listdir
+from util.smart_contract import ensure_wallet, mint_nft, nft, show, own_nft
 
 class Crypto(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    class NFT:
-        def __init__(self, base, transformation):
-            self.base = base
-            self.transformation = transformation
-            color = 1
-            #widmark clan super advanced smart contract
-            self.rarity = randint(0, 100)
-
-            for v in transformation: color*=v
-            #widmark clan super advanced nft hashing algorithm
-            self.address = hex(int(0x1000000)+v)    
-
-        def show():
-            base = Image.open(constants.base)
-            tint = Image.new(mode='RGB', size=(config.NFT_SIZE, config.NFT_SIZE), color = colors)
-            out = ImageChops.multiply(base, tint)
-
-            with BytesIO() as export:
-                out.save(export, 'png')
-                export.seek(0)
-                return discord.File(fp=export, filename=f'{self.address}.png')
-
-    def ensure_wallet(who):
-        if(not config.wallet):
-            config.wallet = {}
-        if(not config.wallet[who.id]):
-            config.wallet[who.id] = []
-        return config.wallet[who.id]
-
-    def mint_nft():
-        """
-        Widmark clan super advanced NFT smart contract
-        """
-        colors = [int(normal(255/2, 255/6))%255 for _ in range(3)]
-        if config.nfts > 255**3:
-            raise AttributeError("All nft's have been minted!")        
-        return NFT(constants.base, colors)
-
-
-
     @slash_command(guild_ids=[config.WIDMARK_CLAN_GUILD_ID], 
         description="Retrieve a user's NFT wallet.")
     async def wallet(self,
         ctx: discord.ApplicationContext,
-        who:  discord.Member=None, nft_id: int=None):
+        user:  discord.Member=None, nft_id: int=None):
         
-        if not who: who = ctx.author
+        if not user: user = ctx.author
 
-        await ctx.defer()
-
-        user_wallet = ensure_wallet(who)
+        user_wallet = ensure_wallet(user, config)
 
         if(user_wallet):
-            nft = user_wallet[(nft_id%len(user_wallet)) or -1]
-            embed = discord.Embed(title=f"{who.nick}\'s NFT wallet", description="on the Widcoin blockchain")
-            file = nft.show()
-            embed.set_image(url="attachment://output.png")
-            embed.set_footer(nft.address)
-            await ctx.respond(embed=embed)
+            nft = user_wallet[(nft_id or -1)% len(user_wallet)]
+            embed = discord.Embed(title=f"{user.nick or user.name}\'s latest NFT!", description=f'Chance to get: {nft["rarity"]}%')
+            file = show(nft, config)
+            embed.set_image(url=f'attachment://{nft["address"]}.png')
+            embed.set_footer(text=str(nft["address"]))
+            await ctx.respond(embed=embed, file=file)
 
         else:
-            await ctx.respond(f'{who.nick} has no NFTs!')
+            await ctx.respond(f'{user.nick or user.name} has no NFTs!')
 
     @slash_command(guild_ids=[config.WIDMARK_CLAN_GUILD_ID], 
-        description="Retrieve a user's NFT wallet.")
+        description="Mint an NFT on the Widcoin blockchain.")
     async def mint(self,
         ctx: discord.ApplicationContext):
         try:
-            nft = mint_nft()
-            user_wallet = ensure_wallet(ctx.author)
-            user_wallet.append(nft)
+            nft = mint_nft(config)
+            user = ctx.author
 
-            embed = discord.Embed(title=f"{who.nick}\'s new NFT!", description=f"Rarity: {nft.rarity}%")
-            file = nft.show()
-            embed.set_image(url="attachment://output.png")
-            embed.set_footer(nft.address)
-            await ctx.respond(embed=embed)
-        except:
+            own_nft(user, nft, config)
+
+            embed = discord.Embed(title=f"{user.nick or user.name} just minted a new NFT!", description=f'Chance to get: {nft["rarity"]}%')
+            file = show(nft, config)
+            embed.set_image(url=f'attachment://{nft["address"]}.png')
+            embed.set_footer(text=str(nft["address"]))
+            await ctx.respond(embed=embed, file=file)
+        except Exception as e:
+            print(e)
             await ctx.respond('A skill issue prevented the minting of an NFT')
 
 
 def setup(bot):
-    bot.add_cog(CSGO(bot))
+    bot.add_cog(Crypto(bot))
