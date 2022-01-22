fe52d49bae808db2edb423e306ecada1f1e1eb10
ericpretzel <ewetzel@ucsb.edu>
2022-01-22 02:17:36 -0800

    add config_template.py

---
+++ config_template.py
@@ -0,0 +1,11 @@
+# Template file
+# Replace TOKEN with your own discord bot's token
+TOKEN = 'abcdefg'
+
+WIDMARK_CLAN_GUILD_ID = 842270794562535434
+
+blacklist = {
+    'YERBA': 171396895661359104,
+    'SAMMICH': 215276133875318785,
+    'ERAKAI': 215272766977474561
+}
