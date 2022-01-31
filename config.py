9b2664fdb3f03f73381facd70361925e5e3a13f6
ericpretzel <ewetzel@ucsb.edu>
2022-01-30 22:39:24 -0800

    clean up config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ TOKEN = os.environ['TOKEN']
 
 # hard-coded channel/server IDs
 WIDMARK_CLAN_GUILD_ID = 842270794562535434
-PRIVATE_PORN_CHANNEL_ID = 882074562529988649
 
 # NFT related
 WALLET = 'data/wallet.json'
@@ -15,10 +14,3 @@ BASE = 'QmWrHFYZarTP2b1qCzG7xB45C7P2f8NvEczUfKDgiv3uEs'
 # Wordle related
 WORDLE_WORD_LIST = 'data/wordle-word-list.txt'
 FIVE_LETTER_WORDS = 'data/five-letter-words.txt'
-
-# users that are noobs
-blacklist = {
-    'YERBA': 171396895661359104,
-    'SAMMICH': 215276133875318785,
-    'ERAKAI': 215272766977474561
-}
--- extension/admin.py
+++ extension/admin.py
@@ -3,21 +3,13 @@ import discord
 from discord.commands import slash_command
 from discord.ext import commands
 
-
 class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    # deletes messages if they are sent by blacklisted user in the given channel
-    @commands.Cog.listener()
-    async def on_message(self, msg: discord.Message):
-        if msg.author.id in config.blacklist.values() and msg.channel.id == config.PRIVATE_PORN_CHANNEL_ID:
-            await msg.delete()
-
     @slash_command(guild_ids=[config.WIDMARK_CLAN_GUILD_ID])
     async def shart(self, ctx: discord.ApplicationContext):
         return await ctx.respond('poo')
-
 
 def setup(bot):
     bot.add_cog(Admin(bot))
