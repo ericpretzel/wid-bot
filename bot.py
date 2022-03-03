9817e740782fe57df4782b8f3b03c0f6a45dd60f
Eric <ewetzel@ucsb.edu>
2022-03-03 02:25:30 -0800

    feedback for loading cogs

--- bot.py
+++ bot.py
@@ -28,10 +28,12 @@ for file in os.listdir('extension'):
     if not file.endswith('.py'):
         continue
     extension = file[:-3]
+    print(f'loading extension.{extension}... ', end='')
     try:
         bot.load_extension('extension.' + extension)
     except Exception as e:
         exc = f'{type(e).__name__}: {e}'
         print(f'Failed to load extension {extension}\n{exc}')
+    print('done')
 
 bot.run(config.TOKEN)
