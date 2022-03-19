dfe3e96948f2c7d48c927433e9ab6d8d449eef73
erakai <kaitinkess@gmail.com>
2022-03-19 18:16:35 -0400

    Add /hf command, re-implement sentimental value, enable Anonymous Polls

--- extension/admin.py
+++ extension/admin.py
@@ -7,9 +7,9 @@ class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    # @slash_command(guild_ids=[config.GUILD_ID])
-    # async def shart(self, ctx: discord.ApplicationContext):
-    #    return await ctx.respond('poo')
+    @slash_command(guild_ids=[config.GUILD_ID])
+    async def shart(self, ctx: discord.ApplicationContext):
+       return await ctx.respond('poo')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
